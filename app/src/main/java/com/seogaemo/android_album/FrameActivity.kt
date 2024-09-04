f2e6d72d0d77f6e16824c8c2b66f4d9a51cea0cc
JangWoojun <woojun0107@naver.com>
2024-09-04 20:06:11 +0900

    feat: FrameActivity 로직 완성

--- app/src/main/java/com/seogaemo/android_album/FrameActivity.kt
+++ app/src/main/java/com/seogaemo/android_album/FrameActivity.kt
@@ -1,17 +1,47 @@
 package com.seogaemo.android_album
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.seogaemo.android_album.databinding.ActivityFrameBinding
+import android.net.Uri
+import android.view.MenuItem
+import com.google.android.material.tabs.TabLayoutMediator
 
 class FrameActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityFrameBinding
+    private lateinit var binding : ActivityFrameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFrameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.toolbar.apply {
+            title = "나만의 앨범"
+            setSupportActionBar(this)
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val images = (intent.getStringArrayExtra("images") ?: emptyArray())
+            .map { uriString ->  FrameItem(Uri.parse(uriString)) }
+        val frameAdapter = FrameAdapter(images)
+
+        binding.viewPager.adapter = frameAdapter
+
+        TabLayoutMediator(
+            binding.tabLayout,
+            binding.viewPager,
+        ) {
+                tab, position ->
+            binding.viewPager.currentItem = tab.position
+        }.attach()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
